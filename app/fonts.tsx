126a06c1a6b788cf50021d8912c3b6e4b6976fd8
xeleniumz <demolismz@windowslive.com>
2024-05-31 16:02:51 +0700

    add assets

---
+++ app/fonts.tsx
@@ -0,0 +1,13 @@
+import { Josefin_Sans, Alata} from 'next/font/google'
+ 
+export const jose_sans = Josefin_Sans({
+    weight: ['300'],
+    subsets: ['latin'],
+    display: 'swap',
+})
+ 
+export const alata = Alata({
+    weight: ['400'],
+    subsets: ['latin'],
+    display: 'swap',
+})
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,19 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata"],
+      },
+      letterSpacing: {
+        widest: ".3em",
       },
     },
   },
